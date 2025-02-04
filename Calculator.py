2c1293cf68399ddbd17b22ca19cc6bf377338ba1
youpu-shizi <shizi01111@gmail.com>
2025-02-04 15:47:36 +0100

    finished but it can only work in CMD

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,11 @@
 print('Hello World! \n')
 print('Sorry for late but this how hereos appears \n')
 print("Don't worry YouPu is here now there is nothing to worry about anymore \n")
-
+answer = input("Do you need a calculator \n")
+if answer == "Yes" or answer == "yes" :
+    print("Welcome to YouPu Calculator")
+else:
+    print("then this not what you wanna use see later friend")
+    name = input("What's your name btw \n")
+    email = input(f"Nice to meet you {name}, let me have your email so I can text you and know what you are intrested about and make it for u in the future")
+    print(f"Got it {name}, I will text you in this {email}, if I'am still alive, See you...")
